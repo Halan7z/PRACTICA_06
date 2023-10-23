f3a2c5ce12b8d29eea444f348968b14c6db3fa32
Alan Flores <alan.flores.c@ucb.edu.bo>
2023-10-22 22:58:07 -0400

    Create Ejercicio_11_06.cpp
    SUBO EL 11, MANDE EL EXE POR ERROR

---
+++ Ejercicio_11_06.cpp
@@ -0,0 +1,53 @@
+// Materia: Programación I, Paralelo 1
+
+// Autor: Alan Franz Flores Campos
+
+// Fecha creación: 22/10/2023
+
+// Fecha modificación: 22/10/2023
+
+// Número de ejericio: 11
+
+/* Problema planteado: Realizar un algoritmo recursivo para elaborar un programa en el que dado un
+entero n > 1, calcule e imprima los elementos correspondientes a la conjetura
+de Ullman (en honor al matemático Ullman) que consiste en lo siguiente:
+• Empiece con cualquier entero positivo.
+• Si es par, divídalo entre 2; si es impar multiplíquelo por 3 y agréguele 1.
+• Obtenga enteros sucesivamente repitiendo el proceso.
+Al final se obtendrá el número 1, independientemente del entero inicial. Por
+ejemplo, cuando el entero inicial es 26, la secuencia será:
+26 13 40 20 10 5 16 8 4 2 1 */
+#include <iostream>
+using namespace std;
+
+void conjeturaUllman(int);
+
+int main()
+{
+    int numInicial;
+    cout<<"Ingrese un entero positivo mayor que 1: ";cin>>numInicial;
+    if (numInicial<=1)
+    {
+        cout<<"El entero inicial debe ser mayor que 1"<<endl;
+        return 1;
+    }
+    cout<<"Secuencia de la conjetura de Ullman: ";
+    conjeturaUllman(numInicial);
+    cout<<endl;
+
+    return 0;
+}
+
+void conjeturaUllman(int n)
+{
+    if (n<=1)
+        cout<<"1 ";
+    else
+    {
+        cout<<n<<" ";
+        if (n%2==0)
+            conjeturaUllman(n/2);
+        else
+            conjeturaUllman(3*n+1);
+    }
+}
